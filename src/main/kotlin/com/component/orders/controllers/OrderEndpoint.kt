9f64dbf344c77f9b9b410e5afeb549fa41490346
Joel Rosario <cirquitz@gmail.com>
2025-09-16 14:57:58 +0530

    Updated versions and wsdl files

--- src/main/kotlin/com/component/orders/controllers/OrderEndpoint.kt
+++ src/main/kotlin/com/component/orders/controllers/OrderEndpoint.kt
@@ -1,30 +1,31 @@
 package com.component.orders.controllers
 
+import com.example.bff.OrderRequest
+import com.example.bff.OrderResponse
 import com.example.orders.CreateOrder
 import com.example.orders.OrderAPIService
-import com.example.orders.OrderRequest
-import com.example.orders.OrderResponse
 import org.springframework.ws.server.endpoint.annotation.Endpoint
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot
 import org.springframework.ws.server.endpoint.annotation.RequestPayload
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload
 import java.io.File
+import java.net.URL
 
 
 @Endpoint
 class OrderEndpoint {
 
     companion object {
-        const val NAMESPACE_URI: String = "http://www.example.com/orders"
-        val ORDER_API_WSDL = File("wsdls/order_api.wsdl").toURI().toURL()
+        const val NAMESPACE_URI: String = "http://www.example.com/bff"
+        val ORDER_API_WSDL: URL = File("wsdls/order_api.wsdl").toURI().toURL()
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "OrderRequest")
     @ResponsePayload
     fun createOrder(@RequestPayload request: OrderRequest): OrderResponse {
         val createOrder = CreateOrder().apply {
-            setProductid(request.productid)
-            setCount(request.count)
+            productid = request.productid
+            count = request.count
         }
 
         val newOrderId = OrderAPIService(ORDER_API_WSDL).orderAPIPort.createOrder(createOrder)
